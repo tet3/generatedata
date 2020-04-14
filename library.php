0c4b5c54883627303767c48ed20ef976f4a0993f
Thomas Taylor <thomas.taylor@slalom.com>
2020-04-14 16:28:55 -0400

    remove deprecated get_magic_quotes_gpc() block

--- library.php
+++ library.php
@@ -44,21 +44,3 @@ require_once(__DIR__ . "/resources/classes/ImportSql.class.php");
 require_once(__DIR__ . "/resources/libs/jsv4.php");
 require_once(__DIR__ . "/resources/libs/smarty/Smarty.class.php");
 require_once(__DIR__ . "/resources/classes/SecureSmarty.class.php");
-
-
-// handle magic quotes
-if (get_magic_quotes_gpc()) {
-    $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
-    while (list($key, $val) = each($process)) {
-        foreach ($val as $k => $v) {
-            unset($process[$key][$k]);
-            if (is_array($v)) {
-                $process[$key][stripslashes($k)] = $v;
-                $process[] = &$process[$key][stripslashes($k)];
-            } else {
-                $process[$key][stripslashes($k)] = stripslashes($v);
-            }
-        }
-    }
-    unset($process);
-}
